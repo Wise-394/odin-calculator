5f193ef0fbd6287597da7c37f0dc056839903f1b
jr <114738222+Wise-394@users.noreply.github.com>
2025-07-01 15:38:48 +0800

    added handle click function

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+//DOM
+const calButtons = document.querySelectorAll(".cal-button")
+const AnswerLabel = document.querySelector("#answer")
+const numberLabel = document.querySelector("#number-label")
+
+//JS VARIABLES
+let numString = ""
+let num1 = ""
+let num2 = ""
+let operation = ""
+
 function add(a, b) {
     console.log(a + b)
 }
 
-function subtract(a, b ) {
+function subtract(a, b) {
     console.log(a - b)
 }
 
@@ -15,18 +26,44 @@ function divide(a, b) {
 }
 
 function operator(a, b, operation) {
-    switch(operation){
+    switch (operation) {
         case "+":
-            add(a,b)
+            add(a, b)
             break;
         case "-":
-            subtract(a,b)
+            subtract(a, b)
             break;
         case "*":
-            multiply(a,b)
+            multiply(a, b)
             break;
         case "/":
-            divide(a,b)
+            divide(a, b)
             break;
     }
 }
+
+//the value of button if its a number or operator
+function handleClick(value) {
+    const specialOperation = ["C", "=",]
+
+    if (!specialOperation.includes(value)) {
+        numString += value
+        numberLabel.textContent = numString
+        return
+    }
+    if (value === specialOperation[0]) {
+        numString = ""
+    }
+    numberLabel.textContent = numString
+
+
+}
+
+
+function main() {
+
+    calButtons.forEach((btn) =>
+        btn.addEventListener("click", () => handleClick(btn.textContent))
+    )
+}
+main()
